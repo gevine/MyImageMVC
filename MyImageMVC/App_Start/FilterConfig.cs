bfdb4a5e469015bf8e8c9e6480c60710e660be13
Levine <levine.esc@gmail.com>
2019-11-12 22:21:45 +0700

    New source code for MVC eProject

---
+++ MyImageMVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MyImageMVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MyImageMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyImageMVC.MvcApplication" Language="C#" %>
